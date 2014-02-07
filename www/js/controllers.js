12f1c3bfab2335f795baea34b2f12ceb36d52644
Juho Vepsalainen <bebraw@gmail.com>
2014-02-07 15:57:49 +0100

    Set up initial views
    Still need attach login, glue it all together and make the app look
    nice. Note that it uses tabs for navigation for now. Sidemenu can be
    added later if needed.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,8 @@
-angular.module('starter.controllers', [])
+'use strict';
 
-
-// A simple controller that fetches a list of data from a service
-.controller('PetIndexCtrl', function($scope, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pets = PetService.all();
-})
-
-
-// A simple controller that shows a tapped item's data
-.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pet = PetService.get($stateParams.petId);
+angular.module('monitor.controllers', [])
+.controller('DashboardCtrl', function($scope, StatisticsService) {
+    $scope.statistics = StatisticsService.get();
+}).controller('PopularCtrl', function($scope) {
+    // TODO
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,26 +1,14 @@
-angular.module('starter.services', [])
+'use strict';
 
-/**
- * A simple example service that returns some data.
- */
-.factory('PetService', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var pets = [
-    { id: 0, title: 'Cats', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-    { id: 1, title: 'Dogs', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
-    { id: 2, title: 'Turtles', description: 'Everyone likes turtles.' },
-    { id: 3, title: 'Sharks', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
-  ];
-
-  return {
-    all: function() {
-      return pets;
-    },
-    get: function(petId) {
-      // Simple index lookup
-      return pets[petId];
-    }
-  }
+angular.module('monitor.services', []).factory('StatisticsService', function() {
+    // TODO: fetch via API
+    return {
+        get: function() {
+            return {
+                transferred: 72.95,
+                cacheHits: 4599850952,
+                nonCacheHits: 18315179
+            };
+        }
+    };
 });
