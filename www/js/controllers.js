686064b55202722eaee2da5c656e8467d9a99d82
Juho Vepsalainen <bebraw@gmail.com>
2014-02-19 17:41:06 +0100

    frontend - Format transferred size in human readable format

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,33 @@ angular.module('monitor.controllers', [])
     dataService.getStatistics().then(function(statistics) {
         $scope.statistics = statistics;
     });
+
+    // http://codeaid.net/javascript/convert-size-in-bytes-to-human-readable-format-(javascript)
+    $scope.bytesToSize = function(bytes, precision) {
+        var kilobyte = 1024;
+        var megabyte = kilobyte * 1024;
+        var gigabyte = megabyte * 1024;
+        var terabyte = gigabyte * 1024;
+
+        if ((bytes >= 0) && (bytes < kilobyte)) {
+            return bytes + ' B';
+
+        } else if ((bytes >= kilobyte) && (bytes < megabyte)) {
+            return (bytes / kilobyte).toFixed(precision) + ' KB';
+
+        } else if ((bytes >= megabyte) && (bytes < gigabyte)) {
+            return (bytes / megabyte).toFixed(precision) + ' MB';
+
+        } else if ((bytes >= gigabyte) && (bytes < terabyte)) {
+            return (bytes / gigabyte).toFixed(precision) + ' GB';
+
+        } else if (bytes >= terabyte) {
+            return (bytes / terabyte).toFixed(precision) + ' TB';
+
+        } else {
+            return bytes + ' B';
+        }
+    };
 }).controller('PopularCtrl', function($scope, dataService) {
     dataService.getPopular().then(function(popular) {
         $scope.popular = popular;
